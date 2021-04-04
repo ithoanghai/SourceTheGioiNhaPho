d55e54c2f9ad0506f60449989e7b57e4ec849f78
kiennguyen <kiennguyen1101@gmail.com>
2021-04-04 14:34:15 +0700

    WIP: search page

--- FunctionModule/pages/views.py
+++ FunctionModule/pages/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.template import loader
-from django.http import HttpResponse
 from django import template
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
-from FunctionModule.listings.choices import price_choices, bedroom_choices, state_choices
+from FunctionModule.cadastral.constants import state_data
+from FunctionModule.listings.choices import price_choices, bedroom_choices
 from FunctionModule.listings.models import Listing
 from FunctionModule.realtors.models import Realtor
-from django.urls import reverse_lazy
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 
 from django.contrib.admin import site
 
@@ -15,13 +25,12 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
         'listings': listings,
-        'state_choices': state_choices,
+        'state_data': state_data,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
     }
 
-    return render(request, 'pages/index.html', context)
-
+    return render(request, 'home/index.html', context)
 
 
 def about(request):
@@ -36,9 +45,12 @@ def about(request):
         'mvp_realtors': mvp_realtors
     }
 
-    return render(request, 'pages/about.html', context)
+    return render(request, 'about/about.html', context)
+
 
 """Admin url here"""
+
+
 def dashboard(request):
     app_list = site.get_app_list(request)
     context = {
@@ -47,12 +59,14 @@ def dashboard(request):
 
     return render(request, 'admin/index.html', context)
 
+
 def layout1(request):
     app_list = site.get_app_list(request)
     context = {
         'available_apps': app_list
     }
     return render(request, 'admin/includes/index-static.html', context)
+
 
 def layout2(request):
     app_list = site.get_app_list(request)
@@ -61,17 +75,20 @@ def layout2(request):
     }
     return render(request, 'admin/includes/index-light.html', context)
 
+
 def error403(request):
     context = {
 
     }
     return render(request, 'admin/page-403.html', context)
 
+
 def error404(request):
     context = {
 
     }
     return render(request, 'admin/page-404.html', context)
+
 
 def error500(request):
     context = {
@@ -91,7 +108,7 @@ def pages(request):
             'segment': load_template,
             'available_apps': app_list
         }
-        #context['segment'] = load_template
+        # context['segment'] = load_template
 
         html_template = loader.get_template(load_template)
         return HttpResponse(html_template.render(context, request))
@@ -106,12 +123,14 @@ def pages(request):
         html_template = loader.get_template('admin/page-500.html')
         return HttpResponse(html_template.render(context, request))
 
+
 def chart(request):
     app_list = site.get_app_list(request)
     context = {
         'available_apps': app_list
     }
     return render(request, 'admin/charts.html', context)
+
 
 def map(request):
     app_list = site.get_app_list(request)
@@ -120,6 +139,7 @@ def map(request):
     }
     return render(request, 'admin/maps.html', context)
 
+
 def profile(request):
     app_list = site.get_app_list(request)
     context = {
@@ -127,12 +147,14 @@ def profile(request):
     }
     return render(request, 'admin/includes/profile.html', context)
 
+
 def table(request):
     app_list = site.get_app_list(request)
     context = {
         'available_apps': app_list
     }
     return render(request, 'admin/tables.html', context)
+
 
 def pageblank(request):
     app_list = site.get_app_list(request)
