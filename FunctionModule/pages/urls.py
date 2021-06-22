8058a9f9ddb81d811069a15e34346140c7fb50f9
ithoanghai <ithoanghai@gmail.com>
2021-06-22 13:41:01 +0700

    fix input box login

--- FunctionModule/pages/urls.py
+++ FunctionModule/pages/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     # Url for Backend
     # path('admin/', admin.site.urls), #for Backend from default lib
     # path('admin/', views.dashboard , name ='dashboard'),
-    path('admin/login/', views.admin_login, name='admin_login'),
+    path('admin/login', views.admin_login, name='admin_login'),
     path('admin/logout/', auth_views.LogoutView.as_view(next_page='/admin/'), name='logout'),
 
     path('<int:layout_id>', views.layout, name='layout'),
