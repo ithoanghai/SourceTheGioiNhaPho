2e01a6fe28f67986eaa2e55a7f387d965abf4a4d
kiennguyen <kiennguyen1101@gmail.com>
2021-06-23 21:55:15 +0700

    added login facebook button

--- FunctionModule/accounts/signals.py
+++ FunctionModule/accounts/signals.py
@@ -1,3 +1,5 @@
+from allauth.account.signals import user_signed_up
+from django.core.handlers.wsgi import WSGIRequest
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
@@ -16,3 +18,8 @@ def realtor_delete_handler(sender, instance: Realtor, **kwargs):
     instance.user.save()
 
 
+@receiver(user_signed_up)
+def user_signed_up_handler(request: WSGIRequest, user, **kwargs):
+    if 'facebook/login/callback' in request.path:
+        user.username = user.email
+        user.save()
