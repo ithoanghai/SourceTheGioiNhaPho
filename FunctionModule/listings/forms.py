b932cf5eb0a1fc9e22fb38e682c8e3278eb11d56
kiennguyen <kiennguyen1101@gmail.com>
2021-07-27 21:59:59 +0700

    compress image before upload

--- FunctionModule/listings/forms.py
+++ FunctionModule/listings/forms.py
@@ -1,3 +1,7 @@
+import sys
+from io import BytesIO
+
+from PIL import Image
 from django import forms
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.forms import Textarea, ModelForm, BaseInlineFormSet
@@ -38,7 +42,7 @@ class ImportListingForm(forms.Form):
 
 
 class ImageForm(ModelForm):
-    photo = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    photo = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
     class Meta:
         model = ListingImage
@@ -51,18 +55,37 @@ INITIAL_FORM_COUNT = 'INITIAL_FORMS'
 MIN_NUM_FORM_COUNT = 'MIN_NUM_FORMS'
 MAX_NUM_FORM_COUNT = 'MAX_NUM_FORMS'
 
+def compress_image(f: InMemoryUploadedFile):
+    if 'png' in f.content_type.lower():
+        image_type = 'PNG'
+    else:
+        image_type = 'JPEG'
+
+    img = Image.open(f.file)
+    output = BytesIO()
+    if f.size > 3029237:
+        quality = 35
+    elif f.size > 1500000:
+        quality = 50
+    else:
+        quality = 75
+    img.save(output, image_type, quality=quality)
+    output.seek(0)
+    new_name = f"{f.name.split('.')[0]}.{image_type.lower()}"
+    new_img = InMemoryUploadedFile(output, 'ImageField', new_name, f.content_type,
+                                   sys.getsizeof(output), None, f.content_type_extra)
+    return new_img
+
 class ImageFormSet(BaseInlineFormSet):
     form = ImageForm
 
     def save_new_objects(self, commit=True):
         try:
             files = self.files.getlist('listingimage_set')
-            # image/png
-            # image/jpeg
-            for f in files: # type: InMemoryUploadedFile
-                # print(f.content_type)
-                ListingImage(photo=f, listing=self.instance).save()
+            for f in files:  # type: InMemoryUploadedFile
+                new_img = compress_image(f)
+                ListingImage(photo=new_img, listing=self.instance).save()
         except AttributeError:
             pass
-            
+
         return super().save_new_objects(commit)
