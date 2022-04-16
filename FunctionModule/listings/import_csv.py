101db2c3b2524f17cc09b474d947453055d15b6f
hoanghai <hoanghai.bdsvn@gmail.com>
2022-04-16 23:51:36 +0700

    code clean realtor and user

--- FunctionModule/listings/import_csv.py
+++ FunctionModule/listings/import_csv.py
@@ -114,8 +114,9 @@ def handle_import(file_path, listing_type):
                         fone = usr.phone
                 for listing in queryset_list:
                     if len(fone) == 10 or len(fone) == 9:
-                        listing.realtor = Realtor.objects.filter(user__phone=fone).first()
-                        if listing.realtor is not None:
+                        realtor_upd = Realtor.objects.filter(user__phone=fone).first()
+                        if realtor_upd is not None:
+                            listing.realtor = realtor_upd
                             listing.save()
                             print(f"cập nhật listing có realtor lỗi sang realtor đầy đủ: {listing.realtor}, phone {fone}")
                     else:
