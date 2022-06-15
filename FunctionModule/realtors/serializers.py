7288cd8aae7117cab679dfcc44d28627c31dd5f0
hoanghai <hoanghai.bdsvn@gmail.com>
2022-06-15 08:22:45 +0700

    code export realtor

--- FunctionModule/realtors/serializers.py
+++ FunctionModule/realtors/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
-from .models import Realtor
+
+from FunctionModule.realtors.models import Realtor
 
 
 class RealtorSerializer(serializers.ModelSerializer):
-    listings = serializers.HyperlinkedRelatedField(many=True,
-                                                read_only=True,
-                                                view_name='listing-detail',
-                                                lookup_field='listing_id')
-    #  Mandatory for UUID serialization
-    realtor_id = serializers.UUIDField()
-
     class Meta:
         model = Realtor
-        exclude = ('id', 'password')
-        read_only_fields = ('created', 'username', 'listings',)
+        exclude = ()
+        read_only_fields = ()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
