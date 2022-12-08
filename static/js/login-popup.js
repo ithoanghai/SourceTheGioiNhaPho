3f680c89ace1ce5941e55919ee096f00f2b26172
hoanghai <ithoanghai@gmail.com>
2022-12-08 18:28:34 +0700

    minicraft css, js,..

--- static/js/login-popup.js
+++ static/js/login-popup.js
@@ -1,25 +1,2 @@
-new Vue({
-    el: "#login-modal",
-    template: "#LoginPopupTemplate",
-    delimiters: [ "[[", "]]" ],
-    data: {
-        isShowPopup: !1
-    },
-    methods: {
-        showPopupModal: function() {
-            this.isShowPopup = !0;
-        },
-        closeModal: function() {
-            this.isShowPopup = !1;
-        },
-        closePopupOnKey: function(t) {
-            "Escape" === t.key && (this.isShowPopup = !1);
-        }
-    },
-    mounted: function() {
-        const t = this;
-        $("a[href='#login']").each(function(e, n) {
-            $(n).on("click", t.showPopupModal);
-        }), document.addEventListener("keydown", this.closePopupOnKey);
-    }
-});
+new Vue({el:"#login-modal",template:"#LoginPopupTemplate",delimiters:["[[","]]"],data:{isShowPopup:!1},methods:{showPopupModal:function(){this.isShowPopup=!0},closeModal:function(){this.isShowPopup=!1},closePopupOnKey:function(t){"Escape"===t.key&&(this.isShowPopup=!1)}},mounted:function(){const t=this
+$("a[href='#login']").each(function(e,n){$(n).on("click",t.showPopupModal)}),document.addEventListener("keydown",this.closePopupOnKey)}})
