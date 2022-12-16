0e101b5ae16a6b9fb87e0445cec502c831a7aa3f
hoanghai <ithoanghai@gmail.com>
2022-12-16 14:58:30 +0700

    edit gulpfile to add source map js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const changed = require('gulp-changed');
 const cleanCSS = require("gulp-clean-css");
 const rev = require('gulp-rev');
 const revFormat = require('gulp-rev-format');
+const sourcemaps = require('gulp-sourcemaps');
 
 const CSS_DESTINATION = 'compiled_staticfiles/css';
 const JS_DESTINATION = 'compiled_staticfiles/js';
@@ -11,14 +12,13 @@ const JS_DESTINATION = 'compiled_staticfiles/js';
 function minifyCSS() {
     return (
         gulp.src("./static/css/*.css", '!./static/css/*.min.css')
-            // .pipe(changed(CSS_DESTINATION))
+            .pipe(changed(CSS_DESTINATION))
             .pipe(cleanCSS())
             .pipe(rev())
             .pipe(revFormat({
                 prefix: '.',
                 suffix: '.min'
             }))
-            .pipe(gulp.dest(CSS_DESTINATION))
             .pipe(rev.manifest('manifest.json'))
             .pipe(gulp.dest(CSS_DESTINATION))
     )
@@ -27,6 +27,7 @@ function minifyCSS() {
 function minifyJS() {
     return (
         gulp.src(['./static/js/*.js', '!./static/js/*.min.js'])
+            .pipe(sourcemaps.init({loadMaps: true, largeFile: true}))
             .pipe(changed(JS_DESTINATION))
             .pipe(uglify())
             .pipe(rev())
@@ -34,7 +35,7 @@ function minifyJS() {
                 prefix: '.',
                 suffix: '.min'
             }))
-            .pipe(gulp.dest(JS_DESTINATION))
+            .pipe(sourcemaps.write())
             .pipe(rev.manifest('manifest.json'))
             .pipe(gulp.dest(JS_DESTINATION))
     )
